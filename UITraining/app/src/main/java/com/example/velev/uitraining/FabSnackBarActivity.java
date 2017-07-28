05741298ef7532cf93d88b8b214ab8cb89ef4a36
Iliya <velev_bg@mail.bg>
2017-07-28 16:27:39 +0300

    Snackbar fab button demo finished

--- UITraining/app/src/main/java/com/example/velev/uitraining/FabSnackBarActivity.java
+++ UITraining/app/src/main/java/com/example/velev/uitraining/FabSnackBarActivity.java
@@ -1,7 +1,11 @@
 package com.example.velev.uitraining;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FabSnackBarActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class FabSnackBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fab_snack_bar);
+
+        final CoordinatorLayout root = (CoordinatorLayout) findViewById(R.id.root);
+
+        Button btnShowShanckbar = (Button) findViewById(R.id.btn_show_snackbar);
+        btnShowShanckbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(root,"simple SnackBar", Snackbar.LENGTH_SHORT)
+                        .setAction("close", new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v) {
+                                // do stuff
+                            }
+                        }).show();
+            }
+        });
     }
 }
