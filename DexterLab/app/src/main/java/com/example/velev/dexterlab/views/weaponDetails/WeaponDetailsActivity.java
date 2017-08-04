0fd2ddf95ba39eb05a9415afc757b13f84dd1afe
Iliya <velev_bg@mail.bg>
2017-08-04 13:01:21 +0300

    Weapons details activity added.

---
+++ DexterLab/app/src/main/java/com/example/velev/dexterlab/views/weaponDetails/WeaponDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.velev.dexterlab.views.weaponDetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.velev.dexterlab.R;
+
+public class WeaponDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weapon_details);
+    }
+}
